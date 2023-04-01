c4bb4b8fd47c55557bdf1bcae3ca8930b6a4a0ed
esaari1 <esaari1@yahoo.com>
2023-04-01 15:26:37 -0400

    Refactor.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { EarthComponent } from './earth/earth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EarthComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'earth', component: EarthComponent },
+      { path: '', redirectTo: '/earth', pathMatch: 'full' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
